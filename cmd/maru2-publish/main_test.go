a98ce245572a119beee9c7c434a8ef2430cad57e
Harry Randazzo <harry@razzle.cloud>
2025-07-22 11:58:40 -0500

    in-memory e2e publish
    Signed-off-by: Harry Randazzo <harry@razzle.cloud>

--- cmd/maru2-publish/main_test.go
+++ cmd/maru2-publish/main_test.go
@@ -4,10 +4,15 @@
 package main
 
 import (
+	"net/http/httptest"
+	"net/url"
 	"os"
 	"testing"
 
+	"github.com/olareg/olareg"
+	olaregcfg "github.com/olareg/olareg/config"
 	"github.com/rogpeppe/go-internal/testscript"
+	"github.com/stretchr/testify/require"
 )
 
 func TestMain(m *testing.M) {
@@ -20,10 +25,25 @@ func TestMain(m *testing.M) {
 }
 
 func TestE2E(t *testing.T) {
+	r := olareg.New(olaregcfg.Config{
+		Storage: olaregcfg.ConfigStorage{
+			StoreType: olaregcfg.StoreMem,
+		},
+	})
+	s := httptest.NewServer(r)
+	t.Cleanup(func() {
+		s.Close()
+		_ = r.Close()
+	})
+
+	serverURL, err := url.Parse(s.URL)
+	require.NoError(t, err)
+
 	testscript.Run(t, testscript.Params{
 		Dir: "testdata",
 		Setup: func(env *testscript.Env) error {
 			env.Setenv("NO_COLOR", "true")
+			env.Setenv("REGISTRY", serverURL.Host)
 			return nil
 		},
 		RequireUniqueNames: true,
