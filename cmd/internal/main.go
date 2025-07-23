48ede91f131f2e816dff06ba4c809bed1785e26c
Harry Randazzo <harry@razzle.cloud>
2025-07-23 15:25:51 -0500

    better example
    Signed-off-by: Harry Randazzo <harry@razzle.cloud>

--- cmd/internal/main.go
+++ cmd/internal/main.go
@@ -5,11 +5,18 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"os"
-	"path/filepath"
+	"os/signal"
+	"slices"
+	"strings"
+	"syscall"
 
+	"github.com/charmbracelet/log"
 	"github.com/spf13/cobra"
 
+	"github.com/defenseunicorns/maru2"
 	maru2cmd "github.com/defenseunicorns/maru2/cmd"
 )
 
@@ -19,30 +26,57 @@ import (
 //
 // the following is the preferred minimal amount of setup needed
 func main() {
-	internalRoot := &cobra.Command{
+	root := &cobra.Command{
 		Use: "internal",
 	}
 
-	// small cobra wrapper
-	wrap := &cobra.Command{
-		Use:                "run",
-		SilenceErrors:      true,
-		SilenceUsage:       true,
-		DisableFlagParsing: true,
-		Run: func(_ *cobra.Command, _ []string) {
-			os.Args = os.Args[1:]
-			code := maru2cmd.Main()
-			os.Exit(code)
-		},
+	cli := maru2cmd.NewRootCmd()
+
+	root.AddCommand(cli)
+
+	// standard setup for context and logger
+	ctx := context.Background()
+
+	ctx, cancel := signal.NotifyContext(ctx, syscall.SIGTERM, syscall.SIGINT)
+	defer cancel()
+
+	var logger = log.NewWithOptions(os.Stderr, log.Options{
+		ReportTimestamp: false,
+	})
+
+	logger.SetStyles(maru2cmd.DefaultStyles())
+	// end context and logger setup
+
+	// run the root, handle the errors
+	// ExecuteContextC is the most preferred method
+	cmd, err := root.ExecuteContextC(ctx)
+	if err != nil {
+		// the below is a copy-paste from maru2, as the formatting of
+		// logging maru2's final error is left up to implementation
+		// this is what users will see if they use maru2 as a standalone CLI
+
+		logger.Print("")
+
+		if errors.Is(cmd.Context().Err(), context.DeadlineExceeded) {
+			logger.Error("task timed out")
+		}
+
+		var tErr *maru2.TraceError
+		if errors.As(err, &tErr) && len(tErr.Trace) > 0 {
+			trace := tErr.Trace
+			slices.Reverse(trace)
+			if len(trace) == 1 {
+				logger.Error(tErr)
+				logger.Error(trace[0])
+			} else {
+				logger.Error(tErr, "traceback (most recent call first)", strings.Join(trace, "\n"))
+			}
+		} else {
+			logger.Error(err)
+		}
 	}
 
-	internalRoot.AddCommand(wrap)
-
-	// multi-call binary w/ wrapper
-	switch filepath.Base(os.Args[0]) {
-	case "maru2":
-		os.Exit(maru2cmd.Main())
-	default:
-		internalRoot.Execute()
-	}
+	// calculate the exit code from the CLI execution
+	code := maru2cmd.ParseExitCode(err)
+	os.Exit(code)
 }
