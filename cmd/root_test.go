a001694ba1cdd600b4e021c0c522c73d0068bd72
Harry Randazzo <harry@razzle.cloud>
2025-09-02 00:18:26 -0500

    parse exit code tests
    Signed-off-by: Harry Randazzo <harry@razzle.cloud>

--- cmd/root_test.go
+++ cmd/root_test.go
@@ -4,10 +4,15 @@
 package cmd_test
 
 import (
+	"errors"
+	"os/exec"
 	"path/filepath"
 	"testing"
 
 	"github.com/rogpeppe/go-internal/testscript"
+	"github.com/stretchr/testify/assert"
+
+	"github.com/defenseunicorns/maru2/cmd"
 )
 
 func TestE2E(t *testing.T) {
@@ -21,4 +26,45 @@ func TestE2E(t *testing.T) {
 		RequireUniqueNames: true,
 		// UpdateScripts:      true,
 	})
+}
+
+func TestParseExitCode(t *testing.T) {
+	tests := []struct {
+		name     string
+		err      error
+		expected int
+	}{
+		{
+			name:     "nil error",
+			err:      nil,
+			expected: 0,
+		},
+		{
+			name:     "generic error",
+			err:      errors.New("some error"),
+			expected: 1,
+		},
+		{
+			name:     "command exit code 0",
+			err:      exec.Command("true").Run(),
+			expected: 0,
+		},
+		{
+			name:     "command exit code 1",
+			err:      exec.Command("false").Run(),
+			expected: 1,
+		},
+		{
+			name:     "command exit code 42",
+			err:      exec.Command("sh", "-c", "exit 42").Run(),
+			expected: 42,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			actual := cmd.ParseExitCode(tt.err)
+			assert.Equal(t, tt.expected, actual)
+		})
+	}
 }
