534f68a8be7eb05c0db6edeedc654acc8de3af53
Harry Randazzo <harry@razzle.cloud>
2025-09-07 13:03:06 -0500

    config dev docs
    Signed-off-by: Harry Randazzo <harry@razzle.cloud>

--- config/location.go
+++ config/location.go
@@ -12,7 +12,10 @@ import (
 // DefaultFileName is the default file name for the config file
 const DefaultFileName = "config.yaml"
 
-// DefaultDirectory returns the default directory for maru2 configuration
+// DefaultDirectory returns the default directory for maru2 configuration ($HOME/.maru2)
+//
+// Currently this relies upon the $HOME environment variable being set
+// In future iterations this may instead leverage the XDG fallback system
 func DefaultDirectory() (string, error) {
 	homeDir, err := os.UserHomeDir()
 	if err != nil {
